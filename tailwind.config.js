c6dc922f863e899ac318b8ae4c9944c992a4bc76
Agung <agungyuni@gmail.com>
2026-05-31 19:06:08 +0800

    p

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   darkMode: 'class',
   content: ["./templates/**/*.html", "./static/**/*.js", "./internal/**/*.go"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2563eb',
+          dark: '#1d4ed8',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
